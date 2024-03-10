c81b9a7a107a91fc4fe4eb55aaac5543b9701b8e
DianaWangui <dianawanguikt048@gmail.com>
2024-03-10 22:08:52 +0300

    update with js for interactivity

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function clickMe() {
     const headerElement = document.getElementById("header-element-list");
     headerElement.style.color = "red";
 }
+function releaseMe() {
+    const headerElement = document.getElementById("header-element-list");
+    headerElement.style.color = "white";
+}
